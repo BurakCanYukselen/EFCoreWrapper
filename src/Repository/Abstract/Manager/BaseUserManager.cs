7b3f1b1f0282348c81aa1b78b63646c4ed0272f7
Burak Can Yükselen <burakcan.yukselen@gmail.com>
2019-11-03 18:29:52 +0300

    abstract usermanager class added

---
+++ src/Repository/Abstract/Manager/BaseUserManager.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Repository.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Abstract.Manager
+{
+    public abstract class BaseUserManager<AppUser> : UserManager<AppUser>
+        where AppUser : IdentityUser
+    {
+        public BaseUserManager(
+            IUserStore<AppUser> store,
+            IOptions<IdentityOptions> optionsAccessor,
+            IPasswordHasher<AppUser> passwordHasher,
+            IEnumerable<IUserValidator<AppUser>> userValidators,
+            IEnumerable<IPasswordValidator<AppUser>> passwordValidators,
+            ILookupNormalizer keyNormalizer,
+            IdentityErrorDescriber errors,
+            IServiceProvider services,
+            ILogger<UserManager<AppUser>> logger) : base(store,
+                                                         optionsAccessor,
+                                                         passwordHasher,
+                                                         userValidators,
+                                                         passwordValidators,
+                                                         keyNormalizer,
+                                                         errors,
+                                                         services,
+                                                         logger)
+        {
+        }
+
+        public async Task<AppUser> GetUserById(string userId)
+        {
+            return await base.FindByIdAsync(userId);
+        }
+        public async Task<AppUser> GetUserByEmail(string email)
+        {
+            return await base.FindByEmailAsync(email);
+        }
+        public async Task<DBResponseModel<AppUser>> CheckUserAndPassword(string email, string password)
+        {
+            var user = await this.GetUserByEmail(email);
+            var checkPassword = await base.CheckPasswordAsync(user, password);
+            if (user == null)
+                return new DBResponseModel<AppUser>(null) { IsSucces = false, ErrorMessage = "User Not Found" };
+            if (checkPassword)
+                return new DBResponseModel<AppUser>(null) { IsSucces = false, ErrorMessage = "Password is not correct" };
+
+            return new DBResponseModel<AppUser>(user) { IsSucces = true};
+        }
+        public async Task<DBResponseModel<AppUser>> CreateUser(AppUser user, string password)
+        {
+            var result = await base.CreateAsync(user, password);
+            if (!result.Succeeded)
+            {
+                var sb = new StringBuilder();
+                sb.AppendJoin(Environment.NewLine, result.Errors.Select(p => p.Description));
+                return new DBResponseModel<AppUser>(null) { IsSucces = false, ErrorMessage = sb.ToString() };
+            }
+            return new DBResponseModel<AppUser>(user) { IsSucces = true };
+        }
+    }
+}
---
+++ src/Repository/Abstract/Manager/Interface/IUserManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Abstract.Manager.Interface
+{
+    public interface IUserManager
+    {
+    }
+}
