b7a9f28f7fab01beeb2a0eb5224663a9aba37413
Burak Can Yükselen <burakcan.yukselen@gmail.com>
2019-11-03 18:30:48 +0300

    service extension added

---
+++ src/Repository/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Repository.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Extensions
+{
+    static class ServiceCollectionExtension
+    {
+        public static void AddEFCoreWrapper<DBContext>(this IServiceCollection services, Action<DbContextOptionsBuilder> optionsAction)
+            where DBContext : DBContextWrapper
+        {
+            services.AddDbContext<DBContext>(optionsAction);
+        }
+
+        public static void AddEFCoreIndetityWrapper<DBContext, AppUser>(this IServiceCollection services, Action<DbContextOptionsBuilder> optionsAction)
+            where DBContext : IdentityDBContextWrapper<AppUser>
+            where AppUser : IdentityUser
+        {
+            services.AddDbContext<DBContext>(optionsAction);
+            services.AddIdentityCore<AppUser>()
+                    .AddEntityFrameworkStores<DbContext>();
+        }
+    }
+}
