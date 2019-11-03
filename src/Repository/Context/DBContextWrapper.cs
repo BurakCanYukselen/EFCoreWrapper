0530e4beb3fbacdeb5f29199515a3b7ed7e95f91
Burak Can Yükselen <burakcan.yukselen@gmail.com>
2019-11-03 18:27:41 +0300

    context classes added

---
+++ src/Repository/Context/DBContextWrapper.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Context
+{
+    public class DBContextWrapper : DbContext
+    {
+        public DBContextWrapper(DbContextOptions<DBContextWrapper> options) : base(options) { }
+    }
+}
---
+++ src/Repository/Context/IdentityDBContextWrapper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Repository.Context
+{
+    public class IdentityDBContextWrapper<AppUser> : IdentityDbContext<AppUser>
+        where AppUser : IdentityUser
+    {
+        public IdentityDBContextWrapper(DbContextOptions<IdentityDBContextWrapper<AppUser>> options) : base(options) { }
+    }
+}
